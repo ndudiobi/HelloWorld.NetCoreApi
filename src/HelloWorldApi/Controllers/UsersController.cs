38896b39a2b4c0db6704f88a5f36d2b69eb75898
Matt Uliasz <matt.uliasz@jet2.com>
2018-10-04 11:11:06 +0100

    Initial commit;

---
+++ src/HelloWorldApi/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public string Get([FromQuery] string name)
+        {
+            return $"Hello {(string.IsNullOrEmpty(name) ? "World" : name)}";
+        }
+    }
+}
